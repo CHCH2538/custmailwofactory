eed5220b578947ec9f554cf8375cffe38d3cd059
Pongpubate Charoensinphutthakhun <pongpubate_ch@hotmail.com>
2021-09-05 20:19:50 +0700

    [Assignment 2] implement factory design pattern

--- src/customermailapplication/CustomerMailApplication.java
+++ src/customermailapplication/CustomerMailApplication.java
@@ -17,23 +17,7 @@ public class CustomerMailApplication {
     /**
      * @param args the command line arguments
      */
-    private Customer customer;
-    public void getCustomerTypeFromUser(String customerType) {
-        switch(customerType) {
-            case "Regular":
-                customer = new RegularCustomer();
-                break;
-            case "Mountain":
-                customer = new MountainCustomer();
-                break;
-            case "Delinquent":
-                customer = new DelinquentCustomer();
-                break;
-        }
-    }
-    public String generateMail() {
-        return customer.createMail();
-    }
+    static private Customer customer;
     
     public static void main(String[] args) {
         CustomerMailApplication app = new CustomerMailApplication();
@@ -42,16 +26,16 @@ public class CustomerMailApplication {
         int type = inp.nextInt();
         switch(type) {
             case 1:
-                app.getCustomerTypeFromUser("Regular");
+            customer = CustomerFactory.createCustomer("Regular");
                 break;
             case 2:
-                app.getCustomerTypeFromUser("Mountain");
+            customer = CustomerFactory.createCustomer("Mountain");
                 break;
             case 3:
-                app.getCustomerTypeFromUser("Delinquent");
+            customer = CustomerFactory.createCustomer("Delinquent");
                 break;
             
         }
-        System.out.println(app.generateMail());        
+        System.out.println(customer.createMail());        
     }
 }
